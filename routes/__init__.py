42b2e4695a359bda1b5660e54c0be7eb9de6e680
Jackstar <114361791+Jackstar-Git@users.noreply.github.com>
2026-03-15 03:06:47 +0100

    Fix

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,7 +5,7 @@ from .projects import projects_blueprint
 from .others import others_blueprint
 from .admin import admin_blueprint
 from .public_api import public_api_blueprint
-from .error import error_blueprint
+from .error import method_not_allowed, page_not_found, access_denied, handle_csrf_error, handle_general_errors
 from .main_routes import home
 
 # Collect all blueprints from globals()
